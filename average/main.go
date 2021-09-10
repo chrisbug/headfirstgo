ff127455585f52cd36745795d9f76cb516bcf2af
ChrisBug <Chris.Buggy@icloud.com>
2021-09-10 11:28:20 +0100

    updated

---
+++ average/main.go
@@ -0,0 +1,22 @@
+// average calculates the varge of sevral numbers
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/headfirstgo/datafile"
+)
+
+func main() {
+	numbers, err := datafile.GetFloats("data.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	var sum float64 = 0
+	for _, number := range numbers {
+		sum += number
+	}
+	sampleCount := float(len(numbers))
+	fmt.Printf("Average ")
+}
