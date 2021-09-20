17c11d5441aa023e9b0470748b9b53e08ea0a272
ChrisBug <Chris.Buggy@icloud.com>
2021-09-20 12:22:51 +0100

    Map chater nearly done

--- maps/main.go
+++ maps/main.go
@@ -7,10 +7,24 @@ import (
 func main() {
 	// var ranks map[string]int
 	// ranks = make(map[string]int)
+	var ok bool
 	ranks := make(map[string]int)
+	var rank int
 	ranks["gold"] = 1
 	ranks["silver"] = 2
 	ranks["bronze"] = 3
-	fmt.Println(ranks["bronze"])
-	fmt.Println(ranks["silver"])
+	rank, ok = ranks["bronze"]
+	fmt.Printf("rank: %d, ok: %v\n", rank, ok)
+	delete(ranks, "bronze")
+	fmt.Printf("rank: %d, ok: %v\n", rank, ok)
+	fmt.Println(ranks)
+
+	isPrime := make(map[int]bool)
+	var prime bool
+	isPrime[5] = true
+	prime, ok = isPrime[5]
+	fmt.Printf("prime: %v, ok: %v\n", prime, ok)
+	delete(isPrime, 5)
+	prime, ok = isPrime[5]
+	fmt.Printf("prime: %v, ok: %v\n", prime, ok)
 }
