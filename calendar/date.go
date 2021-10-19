9238f8cb20c1152646ccec76ec0ae94db2ea1f77
ChrisBug <Chris.Buggy@icloud.com>
2021-10-19 22:44:35 +0100

    un exported

--- calendar/date.go
+++ calendar/date.go
@@ -5,16 +5,16 @@ import (
 )
 
 type Date struct {
-	Year  int
-	Month int
-	Day   int
+	year  int
+	month int
+	day   int
 }
 
 func (d *Date) SetYear(year int) error {
 	if year < 1 {
 		return errors.New("invalid year")
 	}
-	d.Year = year
+	d.year = year
 	return nil
 }
 
@@ -22,7 +22,7 @@ func (d *Date) SetMonth(month int) error {
 	if month < 1 || month > 12 {
 		return errors.New("invalid month has to be between 1 and 12")
 	}
-	d.Month = month
+	d.month = month
 	return nil
 }
 
@@ -30,6 +30,6 @@ func (d *Date) SetDay(day int) error {
 	if day < 1 || day > 31 {
 		return errors.New("Invalid day")
 	}
-	d.Year = day
+	d.day = day
 	return nil
 }
