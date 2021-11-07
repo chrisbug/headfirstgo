1ff6f2f044c00a36e33e59d0463210163626b033
ChrisBug <Chris.Buggy@icloud.com>
2021-11-07 22:56:21 +0000

    adding tests

---
+++ prose/join_test.go
@@ -0,0 +1,11 @@
+package prose
+
+import "testing"
+
+func TestTwoElements(t *testing.T) {
+	t.Error("no test writtent yet")
+}
+
+func TestThreeElements(t *testing.T) {
+	t.Error("no test either")
+}
--- tests/go.mod
+++ tests/go.mod
@@ -2,4 +2,4 @@ module main
 
 go 1.16
 
-require github.com/chrisbug/headfirstgo v0.0.0-20211107213553-6553d49fc882
+require github.com/chrisbug/headfirstgo v0.0.0-20211107221848-ddfceec7d680
