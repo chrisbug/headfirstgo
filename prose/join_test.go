bb0c2ba796759e053cbd5add36122001395aaf4c
ChrisBug <Chris.Buggy@icloud.com>
2021-11-08 19:57:29 +0000

    Fixed test cases

--- prose/join_test.go
+++ prose/join_test.go
@@ -3,9 +3,15 @@ package prose
 import "testing"
 
 func TestTwoElements(t *testing.T) {
-	t.Error("no test writtent yet")
+	list := []string{"apple", "orange"}
+	if JoinWithCommas(list) != "apple and orange" {
+		t.Error("didn't match expected value")
+	}
 }
 
 func TestThreeElements(t *testing.T) {
-	t.Error("no test either")
+	list := []string{"apple", "orange", "pear"}
+	if JoinWithCommas(list) != "apple, orange, and pear" {
+		t.Error("didn't match expected value")
+	}
 }
